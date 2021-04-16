173f1ac6795c8d06ed53c4bec67d12d1888e21ab
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-04-16 18:28:45 -0300

    ✨ jsdoc arrays

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-/** 
- * This is a full name of the user
- * @type {string}
- */
-const fullName = "Name LastName";
+/* eslint-disable no-unused-vars */
+
+/** full name of user
+* @type {string} */
+const fullName = 'Name LastName';
+
+/** array list of ages of users
+* @type {Array<number>} */
+const ages = [19, 20, 21, 22, 23];
+
+/** array list of scores
+ * @type {Array<number | string>} */
+const scoresAndStrings = [10, '10'];
+
+/** array list of users
+ * @type {Array<string>} */
+const users = ['João', 'Pedro', 'Maria'];
+users.push('Ana');
