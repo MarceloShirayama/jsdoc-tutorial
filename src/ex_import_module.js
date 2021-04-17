18b273bf567e229ba4a6c0a683b365f1ac541763
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-04-17 09:12:51 -0300

    ✨ jsdoc module

---
+++ src/ex_import_module.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+
+const myMath = require('./libs/mymath');
+
+const resultSum = myMath.add(2, 3);
+console.log(resultSum);
+
+const resultSub = myMath.sub(2, 3);
+console.log(resultSub);
---
+++ src/libs/mymath.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+
+/**
+ * My math library
+ * @module mymath
+ */
+
+/**
+ * Add two numbers
+ * @param {number} x First number
+ * @param {number} y Second number
+ * @returns {number} Sum: x + y
+ */
+const add = (x, y) => x + y;
+
+/**
+ * Subtract two numbers
+ * @param {number} x First number
+ * @param {number} y Second number
+ * @returns {number} Subtract: x - y
+ */
+const sub = (x, y) => x - y;
+
+module.exports = { add, sub };
